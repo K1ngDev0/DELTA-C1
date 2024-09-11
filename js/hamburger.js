99a2123a76be754c430ef3ed3808a149eaba1e7a
K1ngDev0 <zijlmanslars@gmail.com>
2024-09-11 08:49:07 +0200

    Hamburger functionality
    Ik heb in de hamburger.js een functie toegevoegd voor als je op een knop klikt. Er worden classes toegevoegd aan de classes die er anders uit moeten zien. Zo zorg ik ervoor dat ik de hamburger menu kan laten zien

---
+++ js/hamburger.js
@@ -0,0 +1,6 @@
+function Popup() {
+    var hamburger = document.querySelector(".hamburger");
+    var hamburgerButton = document.querySelector(".hamburgerButton");
+    hamburger.classList.toggle("show");
+    hamburgerButton.classList.toggle("active");
+}
